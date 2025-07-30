d9e8fcf97fdaaa843590a5e790e8fc7ab2ebfed7
guilherme-op7 <netoguilherme989@gmail.com>
2025-07-30 20:03:41 -0300

    subindo projeto pronto

--- connection.js
+++ connection.js
@@ -1,6 +1,5 @@
 import mysql from 'mysql2/promise'
-import dotenv from 'dotenv'
-dotenv.config()
+import 'dotenv/config'
 
 let connection = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
